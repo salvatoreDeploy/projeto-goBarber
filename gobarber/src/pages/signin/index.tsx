f16fee8bd0e5c8e4e4369edfa1d2e3ac160b7a4c
salvatoreDeploy <liderhenrique@gmail.com>
2023-02-25 02:12:41 -0300

    logo import

--- gobarber/src/pages/signin/index.tsx
+++ gobarber/src/pages/signin/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Image } from "react-native";
 
 import { Container } from "./style";
 
+import logoImg from "../../assets/Logo.png";
+
 export function SignIn() {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
 }
